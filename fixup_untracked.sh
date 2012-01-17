fc1aa1a163c774818bfb10c06b58f1ad871c3e5a
Surinder Singh <sursingh@gmail.com>
2012-01-16 18:47:03 -0800

    Added script to add "ignore=untracked" config for all submodules
    also update .git/info/exclude

---
+++ fixup_untracked.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# pathogen updates the tag files in the plugin directories. 
+# Due to this git assumes that we have local changes.
+# 
+# This scripts set the "ignore = untracked" to ignore all new files. 
+# This needs to be run everytime a new submodule is added
+
+git submodule --quiet foreach echo \$name \
+    | xargs -n1 -I{} git config --file .gitmodules submodule.{}.ignore untracked
+
+
+
+git submodule --quiet foreach 'echo "doc/tags*" >.git/info/exclude'
